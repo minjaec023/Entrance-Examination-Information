092ad43b52470afd61405be72be1caf482aa8aaf
ooooorobo <orobos654@gmail.com>
2020-06-14 00:07:52 +0900

    add recommend page
    입력한 표준점수 단순합보다 낮은 학과 모두 보여주게 함
    (show grade는 대학별 가중치 점수 계산한 것 보여줄 때 사용 예정)

--- routes/recom.js
+++ routes/recom.js
@@ -4,9 +4,12 @@ let jwt = require("jsonwebtoken");
 let secretObj = require("../config/jwt");
 const { Router } = require("express");
 const router = express.Router();
+const { Op } = require("sequelize");
 
 const { University } = require("../models");
 const { UnivCriteria } = require("../models");
+const { EngRatio } = require("../models");
+const { Department } = require("../models");
 
 router.use(bodyParser.urlencoded({ extended: false }));
 
@@ -27,6 +30,22 @@ router.get("/recom", function (req, res, next) {
   res.render("recom", { isAuthenticated: isAuthenticated });
 });
 
+router.get("/showrecom", function (req, res, next) {
+  const { kor_p, mat_p, inq_p } = req.query;
+  const { kor_s, mat_s, inq_s } = req.query;
+  let sum_s = parseInt(kor_s) + parseInt(mat_s) + parseInt(inq_s);
+  Department.findAll({
+    include: [{ model: University }],
+    where: { standard_score: { [Op.lt]: sum_s } },
+    order: [["standard_score", "DESC"]],
+  }).then(function (result) {
+    res.render("showrecom", {
+      std_score: sum_s,
+      result,
+    });
+  });
+});
+
 router.get("/criterias/:division", function (req, res, next) {
   let token = req.cookies.user;
   let isAuthenticated;
@@ -41,6 +60,57 @@ router.get("/criterias/:division", function (req, res, next) {
       univ_name: result[0].University.univ_name,
       division: division,
     });
+  });
+});
+
+router.post("/calcgrade", function (req, res, next) {
+  const { kor_s, mat_s, inq_s } = req.body;
+  const { eng } = req.body;
+  let list = [];
+  UnivCriteria.findAll({
+    include: [{ model: University }],
+  }).then(function (results) {
+    for (let result of results) {
+      let calc = {};
+      calc.univ_name = result.University.univ_name;
+      let korean = result.c_korean * kor_s;
+      let math = result.c_math * mat_s;
+      let inquiry = result.c_inquiry * inq_s;
+      let en;
+
+      EngRatio.findOne({ where: { univ_id: result.University.univ_id } })
+        .then(function (en_ratio) {
+          switch (eng) {
+            case "1":
+              en = en_ratio.first_grade;
+              break;
+            case "2":
+              en = en_ratio.second_grade;
+              break;
+            case "3":
+              en = en_ratio.third_grade;
+              break;
+            case "4":
+              en = en_ratio.fourth_grade;
+              break;
+            case "5":
+              en = en_ratio.fifth_grade;
+              break;
+            case "6":
+              en = en_ratio.sixth_grade;
+              break;
+            default:
+              en = 0;
+          }
+          en = en * result.c_english;
+          calc.grade = (korean + math + inquiry + en) / 100;
+        })
+        .then(() => {
+          list.push(calc);
+          if (list.length === results.length)
+            res.render("showgrade", { list: list });
+        });
+    }
   });
 });
 
