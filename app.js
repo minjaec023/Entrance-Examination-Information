2973e83b1e6367647d493df1bc4f64afcef71901
ooooorobo <orobos654@gmail.com>
2020-06-13 22:35:21 +0900

    add criteria list page (showcriteria)
    반영비  가져오기 연습 중...

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const ejs = require('ejs');
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
 
 let jwt = require("jsonwebtoken");
 let secretObj = require("./config/jwt");
 
-const login = require('./routes/login.js');
-const signup = require('./routes/signup.js');
-const recom = require('./routes/recom.js');
-const review = require('./routes/review.js');
-const logout = require('./routes/logout.js');
+const login = require("./routes/login.js");
+const signup = require("./routes/signup.js");
+const recom = require("./routes/recom.js");
+const showrecom = require("./routes/recom.js");
+const review = require("./routes/review.js");
+const logout = require("./routes/logout.js");
 
-const seqeulize = require('./models').sequelize;
+const seqeulize = require("./models").sequelize;
 const app = express();
 seqeulize.sync();
 
@@ -21,28 +22,27 @@ app.use(recom);
 app.use(review);
 app.use(logout);
 
-app.use(express.static(__dirname + '/views'));
-app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({ extended: false}));
+app.use(express.static(__dirname + "/views"));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', function(req, res){
-    let token = req.cookies.user;
-    let isAuthenticated;
-    let decoded;
-    try {
-        decoded = jwt.verify(token, secretObj.secret);
-    } catch (error) {
-        decoded = false;
-    }
-    if(decoded){
-        isAuthenticated = true;
-    }
-    else{
-        isAuthenticated = false;
-    }
-    res.render('main', {isAuthenticated : isAuthenticated});
+app.get("/", function (req, res) {
+  let token = req.cookies.user;
+  let isAuthenticated;
+  let decoded;
+  try {
+    decoded = jwt.verify(token, secretObj.secret);
+  } catch (error) {
+    decoded = false;
+  }
+  if (decoded) {
+    isAuthenticated = true;
+  } else {
+    isAuthenticated = false;
+  }
+  res.render("main", { isAuthenticated: isAuthenticated });
 });
 
-app.listen(3000, function(){
-    console.log('port 3000');
+app.listen(3000, function () {
+  console.log("port 3000");
 });
--- routes/recom.js
+++ routes/recom.js
@@ -1,27 +1,47 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 let jwt = require("jsonwebtoken");
 let secretObj = require("../config/jwt");
+const { Router } = require("express");
 const router = express.Router();
 
-router.use(bodyParser.urlencoded({ extended: false}));
+const { University } = require("../models");
+const { UnivCriteria } = require("../models");
 
-router.get('/recom', function(req, res, next){
-    let token = req.cookies.user;
-    let isAuthenticated;
-    let decoded;
-    try {
-        decoded = jwt.verify(token, secretObj.secret);
-    } catch (error) {
-        decoded = false;
-    }
-    if(decoded){
-        isAuthenticated = true;
-    }
-    else{
-        isAuthenticated = false;
-    }
-    res.render('recom', {isAuthenticated : isAuthenticated});
+router.use(bodyParser.urlencoded({ extended: false }));
+
+router.get("/recom", function (req, res, next) {
+  let token = req.cookies.user;
+  let isAuthenticated;
+  let decoded;
+  try {
+    decoded = jwt.verify(token, secretObj.secret);
+  } catch (error) {
+    decoded = false;
+  }
+  if (decoded) {
+    isAuthenticated = true;
+  } else {
+    isAuthenticated = false;
+  }
+  res.render("recom", { isAuthenticated: isAuthenticated });
+});
+
+router.get("/criterias/:division", function (req, res, next) {
+  let token = req.cookies.user;
+  let isAuthenticated;
+  let division = req.params.division;
+
+  UnivCriteria.findAll({
+    include: [{ model: University }],
+    where: { division: division },
+  }).then(function (result) {
+    res.render("showcriteria", {
+      criterias: result,
+      univ_name: result[0].University.univ_name,
+      division: division,
+    });
+  });
 });
 
 module.exports = router;
